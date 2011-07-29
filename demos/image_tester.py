efa3351066701d46c11b683ce69550732fe4e16b
Bharath Santosh <bsant99@gmail.com>
2011-07-29 00:23:42 -0400

    Color blob tracking added

--- demos/image_tester.py
+++ demos/image_tester.py
@@ -8,18 +8,24 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 class ImageTester:
-  def __init__(self,imageCallBack=None,imageTopic="image_raw"): 
+  def __init__(self,imageCallBack=None,imageTopic="/camera/image_raw",skip_frames=3): 
     rospy.init_node('ImageTester')
     self._imagecall = imageCallBack
     self._imageInput = imageTopic
     self._cvBridge = CvBridge()
     self._imageSub = rospy.Subscriber(self._imageInput,Image,self._imagesubcall)
     self.currFrame = None
+    self.skipFrame = skip_frames
+    self.currFrame = 1
     
   def _imagesubcall(self,image):
-    try:
-      cvImage = self._cvBridge.imgmsg_to_cv(image, "bgr8")
-      if(self._imagecall!=None):
-        self._imagecall(cvImage)
-    except CvBridgeError, e:
-      print "Failed to convert image: %s" %e
+    if(self.skipFrame==self.currFrame):
+      self.currFrame = 1
+      try:
+        cvImage = self._cvBridge.imgmsg_to_cv(image, "bgr8")
+        if(self._imagecall!=None):
+          self._imagecall(cvImage)
+      except CvBridgeError, e:
+        print "Failed to convert image: %s" %e
+    else:
+      self.currFrame += 1
