b687ece92544c245ea775e0d11ba597a64acaf70
Bharath Santosh <bsant99@gmail.com>
2011-07-31 19:53:46 -0400

    commented and added documentation strings to controller

--- src/icreate.py
+++ src/icreate.py
@@ -14,18 +14,29 @@ from cv_bridge import CvBridge, CvBridgeError
 #iCreate Controller
 #==============================================================
 class iCreate:
-  def __init__(self,sensorCallBack=None,imageCallBack=None,imageTopic="image_raw",imageCallBack=None,imageTopic="/camera/image_raw"): 
+  
+  #================================  
+  def __init__(self,sensorCallBack=None,imageCallBack=None,imageTopic="/camera/image_raw"): 
+    """
+      initialize a new icreate and start its ROS node along with its subsribers and services
+        sensorCallBack-the callback that is used when new sensor data is received
+        imageCallBack-the callback that is used when new image data is received
+        imageTopic-the image topic subcribed to
+    """
     rospy.init_node('iCreateController')
     
+    #setup image subscriber
     self._imagecall = imageCallBack
     self._imageInput = imageTopic
     self._cvBridge = CvBridge()
     self._imageSub = rospy.Subscriber(self._imageInput,Image,self._imagesubcall)
     self.currFrame = None
     
+    #setup sensor subscriber
     self._initSensors = False
     self._sensorcall = sensorCallBack
     self._sensorSub = rospy.Subscriber("sensorPacket",SensorPacket,self._sensorsubcall)
+    #initial empty sensor table
     self._sensors = {
       "wheeldropCaster":[],                "wheeldropLeft":[],
       "wheeldropRight":[],                 "bumpLeft":[],
@@ -43,19 +54,30 @@ class iCreate:
       "cliffRightSignal":[],               "homeBase":[],
       "internalCharger":[],                "songNumber":[],
       "songPlaying":[] }
-    while(not self._initSensors):
-      pass
+    while(not self._initSensors): 
+      pass #wait till create driver pushes sensor data
   
-  def _imagesubcall(self,image):
+  #================================  
+  def _imagesubcall(self,image): 
+    """
+      image subscribler callback
+    """
     try:
+      #turn image message into iplimage
       cvImage = self._cvBridge.imgmsg_to_cv(image, "bgr8")
       self.currFrame = cvImage
-      if(self._imagecall!=None):
+      #send image data to exterior callback if it exists
+      if(self._imagecall!=None): 
         self._imagecall(self,cvImage)
     except CvBridgeError, e:
       print "Failed to convert image: %s" %e
-
-  def _sensorsubcall(self,data):
+  
+  #================================  
+  def _sensorsubcall(self,data): 
+    """
+      sensor subscriber callback
+    """
+    #setup sensors table on first sensor packet
     if(not self._initSensors):
       self._sensors = {
         "wheeldropCaster":data.wheeldropCaster,"wheeldropLeft":data.wheeldropLeft,
@@ -75,7 +97,7 @@ class iCreate:
         "internalCharger":data.internalCharger,"songNumber":data.songNumber,
         "songPlaying":data.songPlaying }
       self._initSensors = True
-    else:
+    else: #apply changes to sensor table if the old value has changed
       self._changeAppend("wheeldropCaster", data.wheeldropCaster )
       self._changeAppend("wall", data.wall )
       self._changeAppend("wheeldropRight", data.wheeldropRight )
@@ -108,85 +130,185 @@ class iCreate:
       self._changeAppend("cliffFrontLeftSignal", data.cliffFrontLeftSignal )
       self._changeAppend("cliffFrontRightSignal", data.cliffFrontRightSignal )
   
-  def _changeAppend(self,key,val):
+  #================================
+  def _changeAppend(self,key,val): 
+    """
+      helper method to check changes in sensor table data
+    """
     if self._sensors[key] is not val:
-      self._sensors[key]=val
-      self._onSensorEvent(key,val)
-  
-  def _onSensorEvent(self,sensorID,value):
+      self._sensors[key]=val #set new sensor data in table
+      self._onSensorEvent(key,val) #push new data to sensor event callback
+   
+  #================================ 
+  def _onSensorEvent(self,sensorID,value): 
+    """
+      new sensor data callback
+    """
+    #send sensor data to exterior callback if it exists
     if(self._sensorcall!=None):
       self._sensorcall(self,sensorID,value)
   
-  def sensor(self,key):
-      return self._sensors[key] if key in self._sensors else None
-      
-  def printSensors(self):
+  #================================  
+  def sensor(self,sensor_type): 
+    """
+      returns the sensor value for input sensorname, if it isn't a sensor returns None
+        sensorname-the name of the sensor used
+    """
+    return self._sensors[sensorname] if sensorname in self._sensors else None
+  
+  #================================  
+  def printSensors(self): 
+    """
+      print sensor and sensor data for all sensors
+    """
     for key, value in self._sensors.iteritems():
       print key,":", value
-      
-  def brake(self):
+  
+  #================================  
+  def brake(self): 
+    """
+      stop icreate by calling brake service on icreate driver
+    """
     rospy.wait_for_service('brake')
     try:
+      #call service with parameters
       srvc = rospy.ServiceProxy('brake', Brake)
       response = srvc(True)
       return response.success
     except rospy.ServiceException, e:
       print "Failed to call brake: %s" %e  
-    
-  def turn(self,speed):
+  
+  #================================
+  def turn(self,speed): 
+    """
+      turn icreate by calling turn service on icreate driver with input speed
+        speed-icreate velocity, between -500mm/s(counter-clockwise) and 500mm/s(clockwise)
+    """
     rospy.wait_for_service('turn')
     try:
-      speed = -500 if speed<-500 else (500 if speed>500 else speed)
+      #call service with parameters
+      #constrain speed
+      speed = -500 if speed<-500 else (500 if speed>500 else speed) 
       srvc = rospy.ServiceProxy('turn', Turn)
-      response = srvc(False,speed)
+      response = srvc(False,int(speed))
       return response.success
     except rospy.ServiceException, e:
       print "Failed to call turn: %s" %e
-      
-  def move(self,speed):
+  
+  #================================
+  def move(self,speed): 
+    """
+      move icreate forward or backward by calling tank service on icreate driver using the same speed for both wheels
+        speed-icreate velocity, between -500mm/s(backward) and 500mm/s(forward)
+    """
     rospy.wait_for_service('tank')
     try:
+      #call service with parameters
+      #constrain speed
       speed = -500 if speed<-500 else (500 if speed>500 else speed)
       srvc = rospy.ServiceProxy('tank', Tank)
-      response = srvc(False,speed,speed)
+      response = srvc(False,int(speed),int(speed))
       return response.success
     except rospy.ServiceException, e:
       print "Failed to call move: %s" %e
-      
-  def turnFor(self,tim,speed):
+  
+  #================================
+  def tank(self,left,right): 
+    """
+      move icreate by calling tank service on icreate driver using the left and right speeds for respective wheels
+        left-icreate left wheel velocity, between -500mm/s(backward) and 500mm/s(forward)
+        right-icreate right wheel velocity, between -500mm/s(backward) and 500mm/s(forward)
+    """
+    rospy.wait_for_service('tank')
+    try:
+      #call service with parameters
+      #constrain speeds
+      left = -500 if left<-500 else (500 if left>500 else left)
+      right = -500 if right<-500 else (500 if right>500 else right)
+      srvc = rospy.ServiceProxy('tank', Tank)
+      response = srvc(False,int(left),int(right))
+      return response.success
+    except rospy.ServiceException, e:
+      print "Failed to call move: %s" %e
+  
+  #================================
+  def turnFor(self,duration,speed): 
+    """
+      turn the icreate at input speed for input duration then brake
+        duration-turning time in seconds
+        speed-icreate velocity, between -500mm/s(counter-clockwise) and 500mm/s(clockwise)
+    """
     self.turn(speed)
-    time.sleep(tim)
+    time.sleep(duration)
     self.brake()
   
-  def moveFor(self,tim,speed):
+  #================================
+  def moveFor(self,duration,speed): 
+    """
+      move the icreate forward or backward at input speed for input duration then brake
+        duration-turning time in seconds
+        speed-icreate velocity, between -500mm/s(backward) and 500mm/s(forward)
+    """
     self.move(speed)
-    time.sleep(tim)
+    time.sleep(duration)
     self.brake()
-    
-  def turnAngle(self,angle,speed=130):
-    tim = angle*.0240*100.0/speed
-    self.turnFor(tim,speed)
-
-  def moveDistance(self,dis,speed=130):
-    tim = dis*1000/speed
-    self.moveFor(tim,speed)
-
-  def turnUntil(self,cond,speed=130):
+  
+  #================================  
+  def turnAngle(self,angle,speed=130): 
+    """
+      turn the icreate to the given angle with input speed
+        angle-relative angle in degrees that the icreate will rotate 
+        speed-icreate velocity, between -500mm/s(counter-clockwise) and 500mm/s(clockwise)
+    """ 
+    #hardcoded formula for turn duration given angle, based off 100mm/s
+    duration = angle*.0240*100.0/speed
+    self.turnFor(duration,speed)
+  
+  #================================
+  def moveDistance(self,distance,speed=130): 
+    """
+      move the icreate a given distance forward or backward with a input speed
+        distance-absolute distance in meters
+        speed-icreate velocity, between -500mm/s(backward) and 500mm/s(forward)
+    """
+    duration = distance*1000/speed
+    self.moveFor(duration,speed)
+  
+  #================================
+  def turnUntil(self,condition,speed=130): 
+    """
+      turn the icreate until the condition function returns true, then brake
+        condition-input function that is given the icreate as its parameter
+        speed-icreate velocity, between -500mm/s(counter-clockwise) and 500mm/s(clockwise)
+    """
     self.turn(speed)
-    while (not cond(self)):
+    while (not condition(self)):
       pass
     self.brake()
-    
-  def moveUntil(self,cond,speed=130):
+  
+  #================================  
+  def moveUntil(self,condition,speed=130): 
+    """
+      move the icreate forward or backward until the condition function returns true, then brake
+        condition-input function that is given the icreate as its parameter
+        speed-icreate velocity, between -500mm/s(backward) and 500mm/s(forward)
+    """
     self.move(speed)
-    while (not cond(self)):
+    while (not condition(self)):
       pass
     self.brake()
+
+
 
 #==============================================================
 #Color Blobs
 #==============================================================
-def bloblabel(point,div,pdict,label,blob):
+
+#================================
+def _bloblabel(point,div,pdict,label,blob):
+  """
+   color blob helper function that labels a point and its neighbors
+  """
   pstack = [point] #main stack for blob points
   while (len(pstack)!=0):
     #add point to main blob and give it a label
@@ -203,7 +325,11 @@ def bloblabel(point,div,pdict,label,blob):
         else:
           pstack.append((currpoint[0]+x*div,currpoint[1]+y*div))
 
-def blobrect(blob):
+#================================
+def _blobrect(blob):
+  """
+    color blob helper function to find the bounds of a blob
+  """
   #rectangle properties
   ix=ax=blob[0][0]
   iy=ay=blob[0][1]
@@ -222,10 +348,20 @@ def blobrect(blob):
       iy = p[1]
   return ((ix,iy),(ax,ay),(cx,cy))
 
-def colorblobs(img):
+#================================
+def colorblobs(img,colors,showVideo=False,showDebug=False):
+  """
+    takes in a cv image and a dictionary of colors and picks out all 
+    the color blobs matching the input colors in the image, returns a dictionary of
+    {'color':[array of blob rectangle bounds in form ((x1,y1),(x2,y2),(center of blob x, y))}
+      img-input cv mat image
+      colors-input dictionary of colors in the from {'color':(r,g,b)}
+      showVideo-boolean for displaying image on a cv window
+      showDebug-boolean for showing color points and blobs on a cv window
+  """
   thres = 30 #maximum channel error for color
   (cols,rows) = cv.GetSize(img) #image size
-  colors = {'postit':(151,126,22),'pen':(28,131,116), 'red':(225,15,15)}
+  #colors = {'postit':(151,126,22),'pen':(28,131,116), 'red':(225,15,15)}
   bins = {} #intial color bins
   for k in colors.iterkeys():
     bins[k] = []
@@ -262,7 +398,7 @@ def colorblobs(img):
     for p,l in v.iteritems():
       if(not(l in labels)): #if not labeled
         #label point and all close points into blob and increment label
-        bloblabel(p,div,v,currlabel,preblobs[currlabel])
+        _bloblabel(p,div,v,currlabel,preblobs[currlabel])
         labels.append(currlabel)
         currlabel+=1
         preblobs.append([])
@@ -270,29 +406,34 @@ def colorblobs(img):
     blobthres = 9*3 #minmum number of points in an array
     for b in preblobs:
       if(len(b)>blobthres):
-        blobs.append(blobrect(b))
+        blobs.append(_blobrect(b))
     colorblobs[k] = blobs    
   
-  #show points on screen
-  for k,v in filled.iteritems():
-    ax=ay=0
-    ix=cols
-    iy=rows
-    for p in v:
-      if(p[0]>ax):
-        ax = p[0]
-      elif(p[0]<ix):
-        ix = p[0]
-      if(p[1]>ay):
-        ay = p[1]
-      elif(p[1]<iy):
-        iy = p[1]
-      cv.Circle(img, (p[0],p[1]), 4, (colors[k][2]/2,colors[k][1]/2,colors[k][0]/2))      
+  if(showVideo): #input determines if video is shown
+    if(showDebug): #input determins if points and blobs are outlined
+      #show points on screen
+      for k,v in filled.iteritems():
+        ax=ay=0
+        ix=cols
+        iy=rows
+        for p in v:
+          if(p[0]>ax):
+            ax = p[0]
+          elif(p[0]<ix):
+            ix = p[0]
+          if(p[1]>ay):
+            ay = p[1]
+          elif(p[1]<iy):
+            iy = p[1]
+          cv.Circle(img, (p[0],p[1]), 4, (colors[k][2]/2,colors[k][1]/2,colors[k][0]/2))      
+      
+      #show blobs on screen
+      for k,v in colorblobs.iteritems():
+        for b in v:
+          cv.Rectangle(img, b[0], b[1], (colors[k][2],colors[k][1],colors[k][0]))
+    #show image in cv window
+    cv.NamedWindow("Color",1)
+    cv.ShowImage("Color", img)
+    cv.WaitKey(3)
   
-  #show blobs on screen
-  for k,v in colorblobs.iteritems():
-    for b in v:
-      cv.Rectangle(img, b[0], b[1], (colors[k][2],colors[k][1],colors[k][0]))
-    
-  cv.ShowImage("Color", img)
-  cv.WaitKey(3)
+
