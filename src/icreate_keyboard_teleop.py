1b51df38f583cb4e6d28a189ef7e9e5b29ec94a2
Bharath Santosh <bsant99@gmail.com>
2011-08-02 17:42:13 -0400

    added documentation to the teleoperation node

--- src/icreate_keyboard_teleop.py
+++ src/icreate_keyboard_teleop.py
@@ -4,6 +4,18 @@ import rospy
 from icreate import iCreate
 import curses
 
+"""
+  icreate teleoperation node:
+    allows for one to move around the icreate using keyboard commands
+    its recommended to do this over ssh
+    commands:
+      Use wasd to move and turn the icreate
+      Use f to brake
+      Use z and x to decrease and increases speeds
+      Use ctrl+c to quit
+"""
+
+#change icreate movement with input command
 def setMovement(command,create,speed):
   if(command==ord('w')):
     create.move(speed)    
@@ -18,16 +30,17 @@ if __name__ == '__main__':
   screen = curses.initscr()
   speed = 70
   currMove = ''
+  #set up screen instructions
   screen.border(0)
-  screen.addstr(3,3,"Use wasd to move create")
+  screen.addstr(3,3,"Use wasd to move and turn create")
   screen.addstr(4,3,"Use f to brake")
   screen.addstr(5,3,"Use z and x to decrease and increases speeds")
   screen.addstr(6,3,"Use ctrl+c to quit")
   curses.noecho()
   screen.refresh()
-  create = iCreate()
+  create = iCreate() #make new icreate
   while not rospy.is_shutdown():
-    inp = screen.getch()
+    inp = screen.getch() #get input
     #icreate movement
     if(inp==ord('w') or inp==ord('a') or inp==ord('s') or inp==ord('d')):
       setMovement(inp,create,speed)
@@ -41,7 +54,6 @@ if __name__ == '__main__':
       speed = speed-10 if (speed-10)>=50 else speed
       setMovement(currMove,create,speed)
     elif(inp==ord('x')):
-      speed = speed+10 if (speed-10)>=50 else speed
+      speed = speed+10 if (speed+10)<=140 else speed
       setMovement(currMove,create,speed)
-  
   curses.endwin()
