25ab0ade5d54a82654f3b5162e00269e497c20aa
Bharath Santosh <bsant99@gmail.com>
2011-08-11 18:05:59 -0400

    fixed watcher's delayed brake time

--- src/icreate_watcher.py
+++ src/icreate_watcher.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
   while not rospy.is_shutdown():
     #check if icreate node is  active, if not then brake
     try:
-      rospy.wait_for_service('icreate_shutdown',2)
+      rospy.wait_for_service('icreate_shutdown',5)
     except rospy.ROSException, e:
       _iCreateDriverBrake()
     
